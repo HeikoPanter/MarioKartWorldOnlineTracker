ee04b6701dac170d4d68e99edb7dd75e7af465c4
Heiko <heiko.pantermehl@gmail.com>
2025-06-29 16:12:29 +0200

    - Removed sample fragments - Renamed used fragments to home and statistics

--- app/src/main/java/com/rain/mariokartworldonlinetracker/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/rain/mariokartworldonlinetracker/ui/statistics/StatisticsViewModel.kt
@@ -1,13 +1,13 @@
-package com.rain.mariokartworldonlinetracker.ui.slideshow
+package com.rain.mariokartworldonlinetracker.ui.statistics
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class StatisticsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is statistics Fragment"
     }
     val text: LiveData<String> = _text
 }
