6fa85a684713695f2092ece8016e0d7470fb22d1
Heiko <heiko.pantermehl@gmail.com>
2025-06-29 18:07:40 +0200

    - Added database (room) - Buttons insert a dummy race/knockout

---
+++ app/src/main/java/com/rain/mariokartworldonlinetracker/MarioKartWorldOnlineTrackerApplication.kt
@@ -0,0 +1,8 @@
+package com.rain.mariokartworldonlinetracker
+
+import android.app.Application
+
+class MarioKartWorldOnlineTrackerApplication : Application() {
+    // Lazy Initialization der Datenbank
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
+}
