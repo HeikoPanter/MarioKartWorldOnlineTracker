972dc6b93f98b880d9c66242e8f4e28bd0db82e1
Heiko <heiko.pantermehl@gmail.com>
2025-07-01 23:36:58 +0200

    - New Enum for the track and knockout IDs to store in the database and easily identify them regardless of language - Static map to get information on which route is allowed to drive and which buttons to display/generate

--- app/src/main/java/com/rain/mariokartworldonlinetracker/Enums.kt
+++ app/src/main/java/com/rain/mariokartworldonlinetracker/Enums.kt
@@ -20,3 +20,47 @@ enum class RaceMode {
     _150CC,
     MIRROR
 }
+
+enum class KnockoutCupName {
+    GR1,
+    IR2,
+    MR3,
+    SR4,
+    CR5,
+    AR6,
+    CR7,
+    HR8
+}
+
+enum class TrackName {
+    MBC1,
+    CC2,
+    WSS3,
+    DKSS4,
+    DH5,
+    SGB6,
+    WS7,
+    AF8,
+    DKP9,
+    SVP10,
+    SHS11,
+    WSY12,
+    KTB13,
+    FAO14,
+    PS15,
+    PB16,
+    SSSW17,
+    DDJ18,
+    GBR19,
+    CCF20,
+    DD21,
+    BC22,
+    DBB23,
+    MMM24,
+    CM25,
+    TF26,
+    BC27,
+    AH28,
+    MC29,
+    RR30
+}
