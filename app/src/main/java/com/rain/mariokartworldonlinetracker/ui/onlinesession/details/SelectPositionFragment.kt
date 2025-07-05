c4adde6231fcb3346a1c83e35e1a5d14eb7e93e3
Heiko <heiko.pantermehl@gmail.com>
2025-07-05 11:40:20 +0200

    - Added snackbar to indicate that the result was saved

--- app/src/main/java/com/rain/mariokartworldonlinetracker/ui/onlinesession/details/SelectPositionFragment.kt
+++ app/src/main/java/com/rain/mariokartworldonlinetracker/ui/onlinesession/details/SelectPositionFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.rain.mariokartworldonlinetracker.MarioKartWorldOnlineTrackerApplication
 import com.rain.mariokartworldonlinetracker.MkwotSettings
 import com.rain.mariokartworldonlinetracker.R
@@ -15,6 +17,7 @@ import com.rain.mariokartworldonlinetracker.TrackAndKnockoutHelper.createStandar
 import com.rain.mariokartworldonlinetracker.TrackName
 import com.rain.mariokartworldonlinetracker.data.RaceResultRepository
 import com.rain.mariokartworldonlinetracker.databinding.FragmentSelectPositionBinding
+import com.rain.mariokartworldonlinetracker.ui.onlinesession.EventObserver
 import com.rain.mariokartworldonlinetracker.ui.onlinesession.NewOnlineSessionViewModel
 import com.rain.mariokartworldonlinetracker.ui.onlinesession.NewOnlineSessionViewModelFactory
 import com.rain.mariokartworldonlinetracker.ui.onlinesession.NewOnlineSessionViewModelProvider
@@ -62,12 +65,34 @@ class SelectPositionFragment : Fragment() {
             TrackAndKnockoutHelper::createStandardPositionImageView,
             positionClickHandler
         )
+
+        // Beobachten Sie den Speicherstatus
+        newOnlineSessionViewModel.saveResultStatus.observe(viewLifecycleOwner,
+            EventObserver { success ->
+                if (success) {
+                    // Wichtig: Snackbar benötigt einen CoordinatorLayout oder einen geeigneten View als Anker.
+                    // Verwenden Sie die Wurzel-View des Fragments oder einen CoordinatorLayout, falls vorhanden.
+                    Snackbar.make(
+                        requireView(), "Result saved!",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+
+                    findNavController().navigate(R.id.action_selectPosition_back_to_selectEngineClass)
+                } else {
+                    Snackbar.make(
+                        requireView(), "Error saving the result!.",
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAction("Try again") {
+                        }
+                        .show()
+                }
+            })
     }
 
     fun navigateNext(position: Short?) {
         newOnlineSessionViewModel.setPosition(position)
         newOnlineSessionViewModel.saveNewRace()
-        findNavController().navigate(R.id.action_selectPosition_back_to_selectEngineClass)
 
     }
 }
